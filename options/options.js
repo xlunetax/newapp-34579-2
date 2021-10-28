917a43a8222ae62188fb62789ea62446526e5d3a
Aline Tavares <alaisgomes@gmail.com>
2021-10-28 18:48:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "newapp_34579_2",
+  url: "https://newapp_34579_2.botics.co",
+  api: "https://newapp_34579_2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
